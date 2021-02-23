c2d7e385f4d311dffb51035f501dce58d7673cc7
Mihir <mwadekar2000@gmail.com>
2021-02-23 03:10:00 -0800

    Added Vault routes

--- app.js
+++ app.js
@@ -20,6 +20,14 @@ const GovAlphaMainnetContract = new web3Mainnet.eth.Contract(GovAlphaAbi, GovAlp
 const GovAlphaRinkeby = config.GovAlphaRinkeby;
 const GovAlphaRinkebyContract = new web3Rinkeby.eth.Contract(GovAlphaAbi, GovAlphaRinkeby);
 
+const VaultAbi = config.VaultAbi;
+
+const VaultAbiMainnet = config.VaultMainnet;
+const VaultAbiMainnetContract = new web3Mainnet.eth.Contract(VaultAbi, VaultAbiMainnet);
+
+const VaultAbiRinkeby = config.VaultRinkeby;
+const VaultAbiRinkebyContract = new web3Rinkeby.eth.Contract(VaultAbi, VaultAbiRinkeby);
+
 const app = express();
 const port = process.env.PORT || 80;
 
@@ -105,6 +113,30 @@ app.route('/rinkeby/proposals/').get((req, res) => {
   });
 });
 
+app.route('/rinkeby/vault/etherbalance/').get((req, res) => {
+  VaultAbiRinkebyContract.methods.getVaultETHBalance().call()
+    .then((result) => {
+      etherbalance = result;
+      return res.send(etherbalance);
+    })
+    .catch((error) => {
+      console.error('[rinkeby-etherbalance] error:', error);
+      return res.sendStatus(400);
+    });
+});
+
+app.route('/rinkeby/vault/circulatingsupply/').get((req, res) => {
+  VaultAbiRinkebyContract.methods.getCirculatingSupply().call()
+    .then((result) => {
+      circulatingsupply = result;
+      return res.send(circulatingsupply);
+    })
+    .catch((error) => {
+      console.error('[rinkeby-circulatingsupply] error:', error);
+      return res.sendStatus(400);
+    });
+});
+
 // Mainnet routes
 
 app.route('/mainnet/proposal/:id/state').get((req, res) => {
@@ -114,7 +146,7 @@ app.route('/mainnet/proposal/:id/state').get((req, res) => {
       return res.send(proposal);
     })
     .catch((error) => {
-      console.error('[rinkeby-proposal-state] error:', error);
+      console.error('[mainnet-proposal-state] error:', error);
       return res.sendStatus(400);
     });
 });
@@ -144,11 +176,10 @@ app.route('/mainnet/proposal/:id').get((req, res) => {
     return res.send(proposal[0]);
   })
   .catch((error) => {
-    console.error('[rinkeby-proposal-id] error:', error);
+    console.error('[mainnet-proposal-id] error:', error);
     return res.sendStatus(400);
   });
 });
-
 
 app.route('/mainnet/proposals/').get((req, res) => {
   GovAlphaMainnetContract.getPastEvents('ProposalCreated', {
@@ -174,10 +205,33 @@ app.route('/mainnet/proposals/').get((req, res) => {
     return res.send(proposals);
   })
   .catch((error) => {
-    console.error('[rinkeby-proposals] error:', error);
+    console.error('[mainnet-proposals] error:', error);
     return res.sendStatus(400);
   });
 });
 
+app.route('/mainnet/vault/etherbalance/').get((req, res) => {
+  VaultAbiMainnetContract.methods.getVaultETHBalance().call()
+    .then((result) => {
+      etherbalance = result;
+      return res.send(etherbalance);
+    })
+    .catch((error) => {
+      console.error('[mainnet-etherbalance] error:', error);
+      return res.sendStatus(400);
+    });
+});
+
+app.route('/mainnet/vault/circulatingsupply/').get((req, res) => {
+  VaultAbiMainnetContract.methods.getCirculatingSupply().call()
+    .then((result) => {
+      circulatingsupply = result;
+      return res.send(circulatingsupply);
+    })
+    .catch((error) => {
+      console.error('[mainnet-circulatingsupply] error:', error);
+      return res.sendStatus(400);
+    });
+});
 
 app.listen(port, () => console.log(`API server running on port ${port}`));
